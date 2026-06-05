dcc4358520a82cf6c3e8f17275a0e8b55dc032d1
Cryo <cryo2507@gmail.com>
2026-06-06 00:32:22 +0530

    Initial full stack application

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>
+{
+    res.writeHead(200, {
+        'Content-Type':'application/json'
+    });
+
+    res.end(
+        JSON.stringify({
+            message:"Backend Running"
+        })
+    );
+});
+
+server.listen(3000);
+
+console.log("Server Started");
